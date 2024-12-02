12c1753f0234ca33114e75afe55b115d9fc73542
kittytree <151437342+kittytree@users.noreply.github.com>
2024-12-01 17:06:22 -0800

    cargo fmt

--- 2024/day1.rs
+++ 2024/day1.rs
@@ -4,25 +4,23 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    let input_file = "src/input2.txt".to_string();
-    let left_vector: Vec<i32> ;
+    let input_file = "src/input.txt".to_string();
+    let left_vector: Vec<i32>;
     let right_vector: Vec<i32>;
     (left_vector, right_vector) = get_sorted_vectors(input_file);
 
     get_part_one_distance(&left_vector, &right_vector);
     get_part_two_distance(&left_vector, &right_vector);
-
 }
 
 fn get_part_two_distance(left_vector: &Vec<i32>, right_vector: &Vec<i32>) {
-    let mut count = 0;
     let mut total_distance = 0;
     let mut right_map: HashMap<i32, i32> = HashMap::new();
     for vector in right_vector {
         if right_map.contains_key(&vector) {
             let vector_value = right_map.get(&vector).unwrap();
             right_map.insert(vector.clone(), *vector_value + 1);
-        }else {
+        } else {
             right_map.insert(vector.clone(), 1);
         }
     }
@@ -45,7 +43,7 @@ fn get_part_one_distance(left_vector: &Vec<i32>, right_vector: &Vec<i32>) {
     println!("Total distance part 1: {}", total_distance);
 }
 
-fn get_sorted_vectors(input_file: String)  -> (Vec<i32>, Vec<i32>){
+fn get_sorted_vectors(input_file: String) -> (Vec<i32>, Vec<i32>) {
     let mut left_vector: Vec<i32> = Vec::new();
     let mut right_vector: Vec<i32> = Vec::new();
     if let Ok(lines) = read_lines(input_file) {
@@ -57,7 +55,7 @@ fn get_sorted_vectors(input_file: String)  -> (Vec<i32>, Vec<i32>){
     }
     left_vector.sort();
     right_vector.sort();
-    return (left_vector, right_vector)
+    return (left_vector, right_vector);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
