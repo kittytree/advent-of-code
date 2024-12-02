dd65d5560a8169aaf5ab8ed2ad07672e73c2a059
kittytree <151437342+kittytree@users.noreply.github.com>
2024-12-01 21:32:35 -0800

    oopsie

--- 2024/day1.rs
+++ 2024/day1.rs
@@ -1,76 +1,60 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
     let input_file = "src/input.txt".to_string();
-    let reports: Vec<Vec<i32>>;
-    reports = print_lines(input_file);
-    print_number_of_valid_reports(reports);
+    let left_vector: Vec<i32>;
+    let right_vector: Vec<i32>;
+    (left_vector, right_vector) = get_sorted_vectors(input_file);
+
+    get_part_one_distance(&left_vector, &right_vector);
+    get_part_two_distance(&left_vector, &right_vector);
 }
 
-fn print_number_of_valid_reports(reports: Vec<Vec<i32>>) {
-    let mut count_valid_reports = 0;
-
-    let mut last_entry: i32 = 0;
-    let mut is_first = true;
-
-    let mut is_decreasing: bool = true;
-    let mut is_increasing: bool = true;
-    let mut is_within_one_to_three: bool = true;
-
-    for report in reports {
-        is_first = true;
-        is_decreasing = true;
-        is_increasing = true;
-        is_within_one_to_three = true;
-
-        for entry in report {
-            print!("{} ", entry);
-            if is_first {
-                is_first = false;
-                last_entry = entry;
-            }else{
-                if ((entry - last_entry).abs() >= 1) && ((entry - last_entry).abs() <= 3 ) {
-                    if is_increasing {
-                        if last_entry - entry < 0 {
-                            is_increasing = false;
-                        }
-                    }
-                    if is_decreasing {
-                        if last_entry - entry > 0 {
-                            is_decreasing = false;
-                        }
-                    }
-                }else {
-                    is_within_one_to_three = false;
-                }
-            }
-            last_entry = entry;
+fn get_part_two_distance(left_vector: &Vec<i32>, right_vector: &Vec<i32>) {
+    let mut total_distance = 0;
+    let mut right_map: HashMap<i32, i32> = HashMap::new();
+    for vector in right_vector {
+        if right_map.contains_key(&vector) {
+            let vector_value = right_map.get(&vector).unwrap();
+            right_map.insert(vector.clone(), *vector_value + 1);
+        } else {
+            right_map.insert(vector.clone(), 1);
         }
-        if (is_decreasing || is_increasing) && is_within_one_to_three {
-            count_valid_reports += 1;
-        }
-        println!();
     }
-
-    println!("Found {} valid reports", count_valid_reports);
+    for vector in left_vector {
+        if right_map.contains_key(&vector) {
+            total_distance += right_map.get(&vector).unwrap() * vector;
+        }
+    }
+    println!("Total distance part 2: {}", total_distance);
 }
 
-fn print_lines(input_file: String) -> Vec<Vec<i32>> {
-    let mut reports:Vec<Vec<i32>> = Vec::new();
-    let mut report: Vec<i32> = Vec::new();
+fn get_part_one_distance(left_vector: &Vec<i32>, right_vector: &Vec<i32>) {
+    let mut count = 0;
+    let mut total_distance = 0;
+    while count < left_vector.len(){
+        total_distance += (left_vector[count] - right_vector[count]).abs();
+        count += 1
+    }
+    println!("Total distance part 1: {}", total_distance);
+}
+
+fn get_sorted_vectors(input_file: String) -> (Vec<i32>, Vec<i32>) {
+    let mut left_vector: Vec<i32> = Vec::new();
+    let mut right_vector: Vec<i32> = Vec::new();
     if let Ok(lines) = read_lines(input_file) {
         for line in lines.flatten() {
-            let split_line = line.split(" ").collect::<Vec<&str>>();
-            for item in split_line {
-                report.push(item.parse::<i32>().unwrap());
-            }
-            reports.push(report);
-            report = Vec::new();
+            let stripped_line = line.split("   ").collect::<Vec<&str>>();
+            left_vector.push(stripped_line[0].parse::<i32>().unwrap());
+            right_vector.push(stripped_line[1].parse::<i32>().unwrap());
         }
     }
-    reports
+    left_vector.sort();
+    right_vector.sort();
+    return (left_vector, right_vector);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
