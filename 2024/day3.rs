45b61d9a5ff1b423e500063811d043875b600d7c
kittytree <151437342+kittytree@users.noreply.github.com>
2024-12-03 17:22:32 -0800

    completed part 1

--- 2024/day3.rs
+++ 2024/day3.rs
@@ -1,5 +1,5 @@
 /*
-    - ToDO: Split on regex: mul\(\d+,\d+\)
+    - ToDO: Split on regex: mul\((\d+,\d+)\)
      - add regex to your Cargo.toml if your ide doesn't on use
 */
 
@@ -9,23 +9,28 @@ use std::path::Path;
 use regex::Regex;
 
 fn main() {
-    let input_file = "src/input2.txt".to_string();
-    print_lines(input_file);
+    let input_file = "src/input.txt".to_string();
+    print_part_one_answer(input_file.clone());
 
 }
 
-fn print_lines(input_file: String){
+fn print_part_one_answer(input_file: String){
+    let mut sum_multiplications = 0;
     if let Ok(lines) = read_lines(input_file) {
         for line in lines.flatten() {
-            println!("{}", line);
             let re = Regex::new(r"mul\(\d+,\d+\)").expect("Failed to compile regex");
-            let result = re.captures(line.as_str()).unwrap();
+            let result: Vec<&str> = re.find_iter(line.as_str()).map(|m| m.as_str()).collect();
             for item in result {
-                println!("{}", item);
+                let stripped_prefix = item.strip_prefix("mul(").unwrap();
+                let isolated_inside = stripped_prefix.strip_suffix(")").unwrap();
+                let vec_multiply_arguments = isolated_inside.split(",").collect::<Vec<&str>>();
+                println!("{} x {}", vec_multiply_arguments[0],vec_multiply_arguments[1]);
+                sum_multiplications += vec_multiply_arguments[0].trim().parse::<i32>().unwrap()
+                    * vec_multiply_arguments[1].trim().parse::<i32>().unwrap();
             }
-
         }
     }
+    println!("Sum of multiplications: {}", sum_multiplications);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
