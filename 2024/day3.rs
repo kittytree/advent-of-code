7993ed8682e17a13669825191f7d5bf98d2d5151
kittytree <151437342+kittytree@users.noreply.github.com>
2024-12-03 17:38:00 -0800

    completed day 3

--- 2024/day3.rs
+++ 2024/day3.rs
@@ -11,7 +11,39 @@ use regex::Regex;
 fn main() {
     let input_file = "src/input.txt".to_string();
     print_part_one_answer(input_file.clone());
+    print_part_two_answer(input_file.clone());
 
+}
+
+fn print_part_two_answer(input_file: String) {
+    let mut sum_multiplications = 0;
+    let mut in_do: bool = true;
+    if let Ok(lines) = read_lines(input_file) {
+        for line in lines.flatten() {
+            let re = Regex::new(r"mul\(\d+,\d+\)|don't\(\)|do\(\)").expect("Failed to compile regex");
+            let result: Vec<&str> = re.find_iter(line.as_str()).map(|m| m.as_str()).collect();
+            for item in result {
+                if (item.to_string() == "do()"){
+                    in_do = true;
+                }else if (item.to_string() == "don't()"){
+                    in_do = false;
+                }else{
+                    if (in_do){
+                        let stripped_prefix = item.strip_prefix("mul(").unwrap();
+                        let isolated_inside = stripped_prefix.strip_suffix(")").unwrap();
+                        let vec_multiply_arguments = isolated_inside.split(",").collect::<Vec<&str>>();
+                        println!("{} x {}", vec_multiply_arguments[0],vec_multiply_arguments[1]);
+                        sum_multiplications += vec_multiply_arguments[0].trim().parse::<i32>().unwrap()
+                            * vec_multiply_arguments[1].trim().parse::<i32>().unwrap();
+                    }else{
+                    }
+
+                }
+
+            }
+        }
+    }
+    println!("Sum of multiplications: {}", sum_multiplications);
 }
 
 fn print_part_one_answer(input_file: String){
