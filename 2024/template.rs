ab52e3c058b5437bbdb27758fc71987ce3642a5f
kittytree <151437342+kittytree@users.noreply.github.com>
2024-12-05 07:28:36 -0800

    Create template.rs

---
+++ 2024/template.rs
@@ -0,0 +1,45 @@
+/*
+    - starting code I use for each day. On each day i start a new cargo project
+    - I know this can be streamlined better but I'm still learning :)
+    - maybe after i get stuck on a day ill take a day off to re-structure this stuff to be better
+*/
+
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::time::Instant;
+
+fn main() {
+    let time_start = Instant::now();
+    let input_file = "src/input.txt".to_string();
+
+    print_input(input_file);
+
+    let elapsed_time_to_part_one_complete = time_start.elapsed();
+
+    let elapsed_time_to_part_two_complete = time_start.elapsed();
+    println!(
+        "Time to complete part one: {:.2?}",
+        elapsed_time_to_part_one_complete
+    );
+    println!(
+        "Time to complete part two: {:.2?}",
+        elapsed_time_to_part_two_complete
+    );
+}
+
+fn print_input(input_file: String) {
+    if let Ok(lines) = read_lines(input_file) {
+        for line in lines.flatten() {
+            println!("{}", line);
+        }
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
