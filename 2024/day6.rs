4646269d6ad4ea8f0c495369ba966eb93744e548
kittytree <151437342+kittytree@users.noreply.github.com>
2024-12-06 17:36:21 -0800

    completed input parsing

--- 2024/day6.rs
+++ 2024/day6.rs
@@ -1,9 +1,4 @@
-/*
-    - starting code I use for each day. On each day i start a new cargo project
-    - I know this can be streamlined better but I'm still learning :)
-    - maybe after i get stuck on a day ill take a day off to re-structure this stuff to be better
-*/
-
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -13,8 +8,14 @@ fn main() {
     let time_start = Instant::now();
     let input_file = "src/input.txt".to_string();
 
-    get_starting_hashes(input_file);
+    let row_count: i32;
+    let col_count: i32;
+    let starting_position:((i32, i32), String);
+    let hash_of_obstacles: HashMap<(i32, i32), String>;
 
+    (row_count, col_count, starting_position, hash_of_obstacles) = get_starting_hashes(input_file);
+
+    print_part_one_traveled_distance(row_count, col_count, &starting_position, &hash_of_obstacles);
     let elapsed_time_to_part_one_complete = time_start.elapsed();
 
     let elapsed_time_to_part_two_complete = time_start.elapsed();
@@ -28,16 +29,34 @@ fn main() {
     );
 }
 
-fn get_starting_hashes(input_file: String) {
+fn print_part_one_traveled_distance(row_count: i32, col_count: i32, starting_position: &((i32, i32), String), hash_of_obstacles: &HashMap<(i32, i32), String>) {
+    println!("here")
+}
+
+fn get_starting_hashes(input_file: String) -> (i32, i32, ((i32, i32), String), HashMap<(i32, i32), String>) {
+    let mut row = 0;
+    let mut col = 0;
+    let mut max_col = 0;
+
+    let mut starting_position: ((i32, i32), String) = ((0, 0), String::new());
+    let mut hash_of_obstacles: HashMap<(i32, i32), String> = HashMap::new();
+
     if let Ok(lines) = read_lines(input_file) {
         for line in lines.flatten() {
             for character in line.chars() {
-                if character != '.' && character != '#' {
-                    println!("{}", character);
+                if character == '#' {
+                    hash_of_obstacles.insert((row, col), character.to_string());
+                } else if character != '.'{
+                    starting_position = ((row, col), character.to_string());
                 }
+                col += 1;
             }
+            row += 1;
+            max_col = col;
+            col = 0;
         }
     }
+    (row, max_col, starting_position, hash_of_obstacles)
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
